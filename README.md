697b7c9a75bb7bf10fa1b410018349d888bb320a
maneatingape <44142177+maneatingape@users.noreply.github.com>
2024-11-09 20:49:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Everybody Codes
+
+Solutions for the [Everybody Codes](https://everybody.codes) challenge written in Rust.
+
+## 2024 The Kingdom of Algorithmia
+
+| Quest | Problem | Solution |
+| --- | --- | --- |
---
+++ src/util/heap.rs
@@ -0,0 +1,55 @@
+use std::cmp::Ordering;
+use std::collections::BinaryHeap;
+
+struct Wrapper<K: Ord, V> {
+    key: K,
+    value: V,
+}
+
+impl<K: Ord, V> PartialEq for Wrapper<K, V> {
+    #[inline]
+    fn eq(&self, other: &Self) -> bool {
+        self.key == other.key
+    }
+}
+
+impl<K: Ord, V> Eq for Wrapper<K, V> {}
+
+impl<K: Ord, V> PartialOrd for Wrapper<K, V> {
+    #[inline]
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<K: Ord, V> Ord for Wrapper<K, V> {
+    #[inline]
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.key.cmp(&self.key)
+    }
+}
+
+#[derive(Default)]
+pub struct MinHeap<K: Ord, V> {
+    heap: BinaryHeap<Wrapper<K, V>>,
+}
+
+impl<K: Ord, V> MinHeap<K, V> {
+    pub fn new() -> Self {
+        MinHeap { heap: BinaryHeap::new() }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        MinHeap { heap: BinaryHeap::with_capacity(capacity) }
+    }
+
+    #[inline]
+    pub fn push(&mut self, key: K, value: V) {
+        self.heap.push(Wrapper { key, value });
+    }
+
+    #[inline]
+    pub fn pop(&mut self) -> Option<(K, V)> {
+        self.heap.pop().map(|w| (w.key, w.value))
+    }
+}
