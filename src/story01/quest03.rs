8434692895f9ac23a0b1f7883e5af6f9042632a2
maneatingape <44142177+maneatingape@users.noreply.github.com>
2025-06-07 15:58:22 +0100

    Quest 3

--- src/story01/quest03.rs
+++ src/story01/quest03.rs
@@ -1,11 +1,42 @@
-pub fn part1(_notes: &str) -> i32 {
-    123
+use crate::util::iter::*;
+use crate::util::parse::*;
+
+pub fn part1(notes: &str) -> u64 {
+    parse(notes)
+        .iter()
+        .map(|[x, y]| {
+            let size = x + y - 1;
+            let nx = (x - 1 + 100) % size;
+            (nx + 1) + 100 * (size - nx)
+        })
+        .sum()
 }
 
-pub fn part2(_notes: &str) -> i32 {
-    456
+pub fn part2(notes: &str) -> u64 {
+    search(notes)
 }
 
-pub fn part3(_notes: &str) -> i32 {
-    789
+pub fn part3(notes: &str) -> u64 {
+    search(notes)
+}
+
+fn parse(notes: &str) -> Vec<[u64; 2]> {
+    notes.iter_unsigned::<u64>().chunk::<2>().collect()
+}
+
+fn search(notes: &str) -> u64 {
+    let mut time = 0;
+    let mut factor = 1;
+
+    for [x, y] in parse(notes) {
+        let size = x + y - 1;
+
+        while (x + time) % size != 0 {
+            time += factor;
+        }
+
+        factor *= size;
+    }
+
+    time
 }
--- tests/story01/quest03.rs
+++ tests/story01/quest03.rs
@@ -1,20 +1,36 @@
 use everybody_codes::story01::quest03::*;
 
-const EXAMPLE1: &str = "";
-const EXAMPLE2: &str = "";
-const EXAMPLE3: &str = "";
+const EXAMPLE1: &str = "
+x=1 y=2
+x=2 y=3
+x=3 y=4
+x=4 y=4";
+
+const EXAMPLE2: &str = "\
+x=12 y=2
+x=8 y=4
+x=7 y=1
+x=1 y=5
+x=1 y=3";
+
+const EXAMPLE3: &str = "\
+x=3 y=1
+x=3 y=9
+x=1 y=5
+x=4 y=10
+x=5 y=3";
 
 #[test]
 fn part1_test() {
-    assert_eq!(part1(EXAMPLE1), 123);
+    assert_eq!(part1(EXAMPLE1), 1310);
 }
 
 #[test]
 fn part2_test() {
-    assert_eq!(part2(EXAMPLE2), 456);
+    assert_eq!(part2(EXAMPLE2), 14);
 }
 
 #[test]
 fn part3_test() {
-    assert_eq!(part3(EXAMPLE3), 789);
+    assert_eq!(part3(EXAMPLE3), 13659);
 }
