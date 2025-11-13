5196ee384eade1acff031cae84cae1079ecb5d32
maneatingape <44142177+maneatingape@users.noreply.github.com>
2025-11-13 10:29:02 +0000

    Faster incremental approach

--- src/event2025/quest08.rs
+++ src/event2025/quest08.rs
@@ -4,24 +4,54 @@ pub fn part1(notes: &str) -> usize {
     parse(notes).iter().filter(|(s, e)| e - s == 16).count()
 }
 
-pub fn part2(notes: &str) -> u32 {
-    let threads = parse(notes);
-    threads
-        .iter()
-        .enumerate()
-        .flat_map(|(i, &(s1, e1))| {
-            threads[0..i].iter().map(move |&(s2, e2)| overlap(s1, e1, s2, e2))
-        })
-        .sum()
-}
-
-pub fn part3(notes: &str) -> u32 {
-    let threads = parse(notes);
+pub fn part2(notes: &str) -> i32 {
+    let mut links = vec![vec![]; 257];
+    let mut freq = vec![0; 258];
     let mut result = 0;
 
-    for e1 in 2..257 {
-        for s1 in 1..e1 {
-            let cuts = threads.iter().map(|&(s2, e2)| overlap(s1, e1, s2, e2)).sum();
+    for (start, end) in parse(notes) {
+        links[start].push(end);
+    }
+
+    for (start, ends) in links.iter().enumerate() {
+        for &end in ends {
+            result += freq[start + 1..end].iter().sum::<i32>();
+        }
+
+        for &end in ends {
+            freq[end] += 1;
+        }
+    }
+
+    result
+}
+
+pub fn part3(notes: &str) -> i32 {
+    let mut links = vec![vec![]; 257];
+    let mut delta = vec![0; 258];
+    let mut result = 0;
+
+    for (start, end) in parse(notes) {
+        links[start].push(end);
+        delta[start + 1] += 1;
+        delta[end] -= 1;
+    }
+
+    for start in 1..255 {
+        for &end in &links[start] {
+            delta[end] += 2;
+            delta[end + 1] -= 1;
+        }
+
+        for &end in &links[start - 1] {
+            delta[end] -= 1;
+            delta[end + 1] += 2;
+        }
+
+        let mut cuts = 0;
+
+        for &next in &delta[start + 2..257] {
+            cuts += next;
             result = result.max(cuts);
         }
     }
@@ -29,15 +59,7 @@ pub fn part3(notes: &str) -> u32 {
     result
 }
 
-fn parse(notes: &str) -> Vec<(u32, u32)> {
-    let nails: Vec<_> = notes.iter_unsigned::<u32>().collect();
+fn parse(notes: &str) -> Vec<(usize, usize)> {
+    let nails: Vec<usize> = notes.iter_unsigned().collect();
     nails.windows(2).map(|w| (w[0].min(w[1]), w[0].max(w[1]))).collect()
-}
-
-fn overlap(s1: u32, e1: u32, s2: u32, e2: u32) -> u32 {
-    u32::from(
-        (s1 == s2 && e1 == e2)
-            || (s2 > s1 && s2 < e1 && e2 > e1)
-            || (e2 > s1 && e2 < e1 && s2 < s1),
-    )
 }
