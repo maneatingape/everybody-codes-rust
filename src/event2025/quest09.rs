d52bd4d3e491ed9ba9a69cf534fd9f5e58e35436
maneatingape <44142177+maneatingape@users.noreply.github.com>
2025-11-14 13:10:04 +0000

    Faster approach with early exit

--- src/event2025/quest09.rs
+++ src/event2025/quest09.rs
@@ -1,6 +1,64 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use crate::util::parse::*;
+use std::cmp::Reverse;
+use std::collections::HashSet;
 
+pub fn part1(notes: &str) -> u32 {
+    let mut ducks: Vec<_> = notes.lines().map(Duck::from).collect();
+
+    while !ducks[0].has_parents(&ducks[1], &ducks[2]) {
+        ducks.rotate_left(1);
+    }
+
+    ducks[0].same(&ducks[1]) * ducks[0].same(&ducks[2])
+}
+
+pub fn part2(notes: &str) -> u32 {
+    links(notes).iter().map(|[child, first, second]| child.same(first) * child.same(second)).sum()
+}
+
+pub fn part3(notes: &str) -> u32 {
+    let mut families: Vec<HashSet<_>> = Vec::new();
+
+    for group in links(notes) {
+        let family = families
+            .extract_if(.., |family| group.iter().any(|duck| family.contains(&duck.scales)))
+            .flatten()
+            .chain(group.iter().map(|duck| duck.scales))
+            .collect();
+        families.push(family);
+    }
+
+    families.into_iter().max_by_key(HashSet::len).unwrap().iter().sum()
+}
+
+fn links(notes: &str) -> Vec<[Duck; 3]> {
+    let ducks: Vec<_> = notes.lines().map(Duck::from).collect();
+    let mut order = ducks.clone();
+    let mut links = Vec::new();
+
+    'outer: for child in ducks {
+        order.sort_unstable_by_key(|duck| Reverse(child.same(duck)));
+
+        for (i, first) in order.iter().enumerate().skip(2) {
+            for second in &order[1..i] {
+                if child.same(first) + child.same(second) < child.size {
+                    continue 'outer;
+                }
+
+                if child.has_parents(first, second) {
+                    links.push([child, *first, *second]);
+                    continue 'outer;
+                }
+            }
+        }
+    }
+
+    links
+}
+
+#[derive(Clone, Copy)]
 struct Duck {
+    scales: u32,
     size: u32,
     lo: u128,
     hi: u128,
@@ -8,109 +66,23 @@ struct Duck {
 
 impl Duck {
     fn from(line: &str) -> Self {
-        let (_, suffix) = line.split_once(':').unwrap();
+        let (prefix, suffix) = line.split_once(':').unwrap();
 
+        let scales = prefix.unsigned();
         let size = suffix.len() as u32;
         let lo = suffix.bytes().fold(0, |lo, b| (lo << 1) | u128::from(b == b'G' || b == b'T'));
         let hi = suffix.bytes().fold(0, |hi, b| (hi << 1) | u128::from(b == b'C' || b == b'T'));
 
-        Duck { size, lo, hi }
+        Duck { scales, size, lo, hi }
     }
 
-    fn similar(&self, other: &Duck) -> u32 {
+    fn same(&self, other: &Duck) -> u32 {
         self.size - ((self.lo ^ other.lo) | (self.hi ^ other.hi)).count_ones()
     }
 
-    fn parents(&self, first: &Duck, second: &Duck) -> bool {
+    fn has_parents(&self, first: &Duck, second: &Duck) -> bool {
         let lo = (self.lo ^ first.lo) & (self.lo ^ second.lo);
         let hi = (self.hi ^ first.hi) & (self.hi ^ second.hi);
         (lo | hi) == 0
     }
-}
-
-pub fn part1(notes: &str) -> u32 {
-    let mut ducks: Vec<_> = notes.lines().map(Duck::from).collect();
-
-    while !ducks[0].parents(&ducks[1], &ducks[2]) {
-        ducks.rotate_left(1);
-    }
-
-    ducks[0].similar(&ducks[1]) * ducks[0].similar(&ducks[2])
-}
-
-pub fn part2(notes: &str) -> u32 {
-    let ducks: Vec<_> = notes.lines().map(Duck::from).collect();
-    let mut result = 0;
-
-    'outer: for child in 0..ducks.len() {
-        for first in 0..ducks.len() {
-            for second in first + 1..ducks.len() {
-                if first != child
-                    && second != child
-                    && ducks[child].parents(&ducks[first], &ducks[second])
-                {
-                    result +=
-                        ducks[child].similar(&ducks[first]) * ducks[child].similar(&ducks[second]);
-                    continue 'outer;
-                }
-            }
-        }
-    }
-
-    result
-}
-
-pub fn part3(notes: &str) -> usize {
-    let ducks: Vec<_> = notes.lines().map(Duck::from).collect();
-
-    let mut graph = HashMap::new();
-    let mut link = |from, to| {
-        graph.entry(from).or_insert_with(Vec::new).push(to);
-        graph.entry(to).or_insert_with(Vec::new).push(from);
-    };
-
-    'outer: for child in 0..ducks.len() {
-        for first in 0..ducks.len() {
-            for second in first + 1..ducks.len() {
-                if first != child
-                    && second != child
-                    && ducks[child].parents(&ducks[first], &ducks[second])
-                {
-                    link(first, child);
-                    link(second, child);
-                    continue 'outer;
-                }
-            }
-        }
-    }
-
-    let mut todo = VecDeque::new();
-    let mut remaining: HashSet<_> = (0..ducks.len()).collect();
-    let mut groups = Vec::new();
-
-    while let Some(&start) = remaining.iter().next() {
-        todo.push_back(start);
-        remaining.remove(&start);
-
-        let mut size = 0;
-        let mut scales = 0;
-
-        while let Some(duck) = todo.pop_front() {
-            size += 1;
-            scales += duck;
-
-            if let Some(relatives) = graph.get(&duck) {
-                for &next in relatives {
-                    if remaining.remove(&next) {
-                        todo.push_back(next);
-                    }
-                }
-            }
-        }
-
-        groups.push((size, scales));
-    }
-
-    let &(size, scales) = groups.iter().max_by_key(|(size, _)| size).unwrap();
-    size + scales
 }
